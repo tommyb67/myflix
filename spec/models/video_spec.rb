9ae0da16307011dcd861d14d50b40faf035e5b4a
Tom Brennan <tommybrennan@gmail.com>
2014-10-27 09:32:50 -0400

    implemented the video self search by title method using TDD

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -17,10 +17,28 @@ describe Video do
       family = Video.create(title: "Family Guy", description: "Adult Cartoon")
       futurama = Video.create(title: "Futurama", description: "Space fantasy")
 
-      expect(Video.search_by_title("Fmaily Guy")).to eq(["Family Guy"])
+      expect(Video.search_by_title("Futurama")).to eq([futurama])
     end
 
-    it "returns an array of one video for a partial match"
-    it "returns an array of all matches ordered by created_at"
+    it "returns an array of one video for a partial match" do
+      family = Video.create(title: "Family Guy", description: "Adult Cartoon")
+      futurama = Video.create(title: "Futurama", description: "Space fantasy")
+
+      expect(Video.search_by_title("Family")).to eq([family])
+    end
+
+    it "returns an array of all matches ordered by created_at" do
+      family = Video.create(title: "Family Guy", description: "Adult Cartoon", created_at: 1.day.ago)
+      all_family = Video.create(title: "All in the Family", description: "unpc queens family")
+
+      expect(Video.search_by_title("Family")).to eq([all_family, family])
+    end
+
+     it "returns an empty array for a search with an empty string" do
+      family = Video.create(title: "Family Guy", description: "Adult Cartoon")
+      futurama = Video.create(title: "Futurama", description: "Space fantasy")
+
+      expect(Video.search_by_title("")).to eq([])
+    end
   end
 end
